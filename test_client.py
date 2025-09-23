84b1471df902f2bdccc159d1dd9ddcdebc69566c
Nathanavy <161822371+Nathanavy@users.noreply.github.com>
2025-09-23 18:08:42 +0100

    Update test_client.py

--- test_client.py
+++ test_client.py
@@ -1,18 +1,13 @@
-import json
-from app import app
+import requests
 
-def test_predict():
-    client = app.test_client()
-    response = client.post(
-        "/predict",
-        json={
-            "DayOfWeek": 3,
-            "Holiday_Flag": 0,
-            "Year": 2012,
-            "lag_1": 20000,
-            "rolling_mean_4": 21000
-        }
-    )
-    data = response.get_json()
-    assert "prediction" in data
-    assert isinstance(data["prediction"], float)
+url = "http://127.0.0.1:5000/predict"
+data = {
+    "DayOfWeek": 3,
+    "Holiday_Flag": 0,
+    "Year": 2012,
+    "lag_1": 20000,
+    "rolling_mean_4": 21000
+}
+
+response = requests.post(url, json=data)
+print(response.json())
